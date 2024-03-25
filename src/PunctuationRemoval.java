df67049c6120f83f31c480695d42abf6707554f4
hafsa234245 <hafsaakter7534@gmail.com>
2024-03-25 10:55:28 +0600

    PunctuationRemoval solved by java project

---
+++ src/PunctuationRemoval.java
@@ -0,0 +1,12 @@
+import java.util.scanner;
+public class PunctuationRemoval {
+    public static void main(String][]args){
+        scanner scanner=new scanner(System.in);
+        system .out.print("Enter any number with comma:");
+        string inputNumber=scanner.next();
+        int len=inputNumber.length();
+        string firstsome=inputNumber.substring(0,len-4);
+        String lastThree=inputNumber.substring(beingindex:len-3);
+        sustem.out,println(firstsome+lastsome);
+    }
+}
